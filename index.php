f764da1d6a7fd97bd14943eff970fc3ac6df1f32
AneruMarlow <gruviafromm@gmail.com>
2019-06-25 19:23:57 +0300

    Add form

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo 'Hello, world';
+?>
+
+<form action="action.php" method="post">
+    <input type="text" name="Name">
+    <input type="text" name="Description">
+    <input type="date" name="Created">
+    <input type="submit" name="Save">
+</form>
+
+
