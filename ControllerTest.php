9eb57ce7e5e5f47ebc9a73d1e744cf3fb3f146c8
AneruMarlow <gruviafromm@gmail.com>
2019-07-10 21:55:29 +0300

    test

---
+++ ControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
+
+class ControllerTest extends TestCase
+{
+    public function testRead()
+    {
+        $httpClient = HttpClient::create(['base_uri' => 'http://127.0.0.1:8000',]);
+        $response = $httpClient->request('GET', '/read.php');
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+    public function testCreate()
+    {
+        $httpClient = HttpClient::create(['base_uri' => 'http://127.0.0.1:8000',]);
+        $response = $httpClient->request('GET', '/create.php');
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+    public function testUpdate()
+    {
+        $httpClient = HttpClient::create(['base_uri' => 'http://127.0.0.1:8000',]);
+        $response = $httpClient->request('GET', '/update.php');
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+    public function testDelete()
+    {
+        $httpClient = HttpClient::create(['base_uri' => 'http://127.0.0.1:8000',]);
+        $response = $httpClient->request('GET', '/delete.php');
+        $this->assertEquals($response->getStatusCode(), 302);
+    }
+}
